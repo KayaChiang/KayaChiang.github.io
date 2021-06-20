cf76c9da9c7a86ccdecb154716df1dc13e74344c
KayaChiang <kaya93226@gmail.com>
2021-06-20 22:42:21 +0800

    0620_2230ver.

--- JS_homework/Apple_filter.js
+++ JS_homework/Apple_filter.js
@@ -1,23 +1,20 @@
-let iPad = {
-    Service: [
-        { service: 'Apple_img/service.svg', s_text: '快速且免額外付費的運送服務' },
-        { service: 'Apple_img/customize.svg', s_text: '免額外付費的個人化鐫刻服務' },
-        { service: 'Apple_img/return.svg', s_text: '免額外付費輕鬆退貨' },
-    ],
-    Appearance: [
-        { color: '太空灰色', color_img: 'Apple_img/iPad/finish-spacegray-201903.png', product_Url: 'Apple_img/iPad/ipad-2020-hero-space-wifi-select.png' },
-        { color: '銀色', color_img: 'Apple_img/iPad/finish-silver-201903.png', product_Url: 'Apple_img/iPad/ipad-2020-hero-silver-wifi-select.png' },
-        { color: '金色', color_img: 'Apple_img/iPad/finish-gold-201903.png', product_Url: 'Apple_img/iPad/ipad-2020-hero-gold-wifi-select.png' },
-    ],
-    Storage:
-        [
-            { size: '32GB', price: 'NT$10,500' },
-            { size: '128GB', price: 'NT$13,500' },
-        ],
-    Add_on: [
-        { network: 'Wi-Fi', price: 'NT$10,500' },
-        { network: 'Wi-Fi + 行動網路', price: 'NT$14,800' }
-    ]
-}
-        
+let iPad = [
+    // 太空灰
+    { 'sku': '001_gre', 'color': '太空灰色', 'color_img': 'Apple_img/iPad/finish-spacegray-201903.png', 'product_Url': 'Apple_img/iPad/ipad-2020-hero-space-wifi-select.png', 'size': '32GB', 'network': 'Wi-Fi', 'price': 'NT$10,500' },
+    { 'sku': '002_gre', 'color': '太空灰色', 'color_img': 'Apple_img/iPad/finish-spacegray-201903.png', 'product_Url': 'Apple_img/iPad/ipad-2020-hero-space-wifi-select.png', 'size': '128GB', 'network': 'Wi-Fi', 'price': 'NT$13,500' },
+    { 'sku': '003_gre', 'color': '太空灰色', 'color_img': 'Apple_img/iPad/finish-spacegray-201903.png', 'product_Url': 'Apple_img/iPad/ipad-2020-hero-space-wifi-select.png', 'size': '32GB', 'network': 'Wi-Fi + 行動網路', 'price': 'NT$14,800' },
+    { 'sku': '004_gre', 'color': '太空灰色', 'color_img': 'Apple_img/iPad/finish-spacegray-201903.png', 'product_Url': 'Apple_img/iPad/ipad-2020-hero-space-wifi-select.png', 'size': '128GB', 'network': 'Wi-Fi + 行動網路', 'price': 'NT$17,800' },
 
+    // 銀色
+    { 'sku': '005_sil', 'color': '銀色', 'color_img': 'Apple_img/iPad/finish-silver-201903.png', 'product_Url': 'Apple_img/iPad/ipad-2020-hero-space-wifi-select.png', 'size': '32GB', 'network': 'Wi-Fi', 'price': 'NT$10,500' },
+    { 'sku': '006_sil', 'color': '銀色', 'color_img': 'Apple_img/iPad/finish-silver-201903.png', 'product_Url': 'Apple_img/iPad/ipad-2020-hero-space-wifi-select.png', 'size': '128GB', 'network': 'Wi-Fi', 'price': 'NT$13,500' },
+    { 'sku': '007_sil', 'color': '銀色', 'color_img': 'Apple_img/iPad/finish-silver-201903.png', 'product_Url': 'Apple_img/iPad/ipad-2020-hero-space-wifi-select.png', 'size': '32GB', 'network': 'Wi-Fi + 行動網路', 'price': 'NT$14,800' },
+    { 'sku': '008_sil', 'color': '銀色', 'color_img': 'Apple_img/iPad/finish-silver-201903.png', 'product_Url': 'Apple_img/iPad/ipad-2020-hero-space-wifi-select.png', 'size': '128GB', 'network': 'Wi-Fi + 行動網路', 'price': 'NT$17,800' },
+
+    // 金色
+    { 'sku': '009_ro', 'color': '金色', 'color_img': 'Apple_img/iPad/finish-gold-201903.png', 'product_Url': 'Apple_img/iPad/ipad-2020-hero-gold-wifi-select.png', 'size': '32GB', 'network': 'Wi-Fi', 'price': 'NT$10,500' },
+    { 'sku': '010_ro', 'color': '金色', 'color_img': 'Apple_img/iPad/finish-gold-201903.png', 'product_Url': 'Apple_img/iPad/ipad-2020-hero-gold-wifi-select.png', 'size': '128GB', 'network': 'Wi-Fi', 'price': 'NT$13,500' },
+    { 'sku': '011_ro', 'color': '金色', 'color_img': 'Apple_img/iPad/finish-gold-201903.png', 'product_Url': 'Apple_img/iPad/ipad-2020-hero-gold-wifi-select.pngg', 'size': '32GB', 'network': 'Wi-Fi + 行動網路', 'price': 'NT$14,800' },
+    { 'sku': '012_ro', 'color': '金色', 'color_img': 'Apple_img/iPad/finish-gold-201903.png', 'product_Url': 'Apple_img/iPad/ipad-2020-hero-gold-wifi-select.png', 'size': '128GB', 'network': 'Wi-Fi + 行動網路', 'price': 'NT$17,800' },
+
+]
