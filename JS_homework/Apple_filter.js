198ce356381b36d1a402a7b87b615f45dd5f11a7
KayaChiang <kaya93226@gmail.com>
2021-06-20 21:04:27 +0800

    0620_ver.

---
+++ JS_homework/Apple_filter.js
@@ -0,0 +1,23 @@
+let iPad = {
+    Service: [
+        { service: 'Apple_img/service.svg', s_text: '快速且免額外付費的運送服務' },
+        { service: 'Apple_img/customize.svg', s_text: '免額外付費的個人化鐫刻服務' },
+        { service: 'Apple_img/return.svg', s_text: '免額外付費輕鬆退貨' },
+    ],
+    Appearance: [
+        { color: '太空灰色', color_img: 'Apple_img/iPad/finish-spacegray-201903.png', product_Url: 'Apple_img/iPad/ipad-2020-hero-space-wifi-select.png' },
+        { color: '銀色', color_img: 'Apple_img/iPad/finish-silver-201903.png', product_Url: 'Apple_img/iPad/ipad-2020-hero-silver-wifi-select.png' },
+        { color: '金色', color_img: 'Apple_img/iPad/finish-gold-201903.png', product_Url: 'Apple_img/iPad/ipad-2020-hero-gold-wifi-select.png' },
+    ],
+    Storage:
+        [
+            { size: '32GB', price: 'NT$10,500' },
+            { size: '128GB', price: 'NT$13,500' },
+        ],
+    Add_on: [
+        { network: 'Wi-Fi', price: 'NT$10,500' },
+        { network: 'Wi-Fi + 行動網路', price: 'NT$14,800' }
+    ]
+}
+        
+
